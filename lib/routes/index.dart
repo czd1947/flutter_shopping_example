1d579910db9caf1b72b928f4152ea40c5df2cbfb
小丸子驾到 <939837530@qq.com>
2025-12-24 16:08:46 +0800

    初始搭建路由和页面组件

--- lib/routes/index.dart
+++ lib/routes/index.dart
@@ -1,17 +1,13 @@
+import 'package:flu_web_pro01/pages/Login/index.dart';
+import 'package:flu_web_pro01/pages/Main/index.dart';
 import 'package:flutter/material.dart';
 
 // 返回App根级组件
 Widget getRootWidget() {
-  return MaterialApp(
-    routes: {},
-    home: Scaffold(
-      appBar: AppBar(title: const Text('Flutter Web Pro01')),
-      body: const Center(child: Text('Hello Flutter Web Pro01!')),
-    ),
-  );
+  return MaterialApp(initialRoute: '/', routes: getRootRoutes());
 }
 
 // 返回该App 的路由配置
 Map<String, Widget Function(BuildContext)> getRootRoutes() {
-  return {};
+  return {'/': (context) => MainPage(), '/login': (context) => LoginPage()};
 }
